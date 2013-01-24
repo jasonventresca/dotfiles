6bd4707169831d416c3f5ab130a9e35310be1922
Ubuntu <ubuntu@ip-10-202-67-19.ec2.internal>
2013-01-24 02:14:03 +0000

    made the whole process totally bootstrappable, and tidied up a few things

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,34 @@
 # Use this file to (re)build a familiar command line env
-# run "wget --no-check-certificate https://github.com/paulkiernan/dotfiles/raw/master/bootstrap.sh -O - | sh"
+# run "wget --no-check-certificate https://github.com/jasonventresca/dotfiles/raw/master/bootstrap.sh -O - | sh"
 
 # Don't forget the SSH keys.
 # Need to sudo to sucessfully apt-get
 
 # Vars
-dir=~/dotfiles
+dir=~/dotfiles/dotfiles
 old_dir=~/dotfiles.old
-files=".bashrc .gitconfig .tmux.conf .vim .vimrc"
+files="bashrc gitconfig tmux.conf vim vimrc"
 
-echo "Creating the local directory for dotfiles"
-mkdir ~/dotfiles/
-
-# Install essential dev tools
 echo "Installing essential dev tools"
-sudo apt-get -y install vim git-core
+sudo apt-get -y install vim git-core curl tmux
 
 # Install these dotfiles locally
 echo "Cloning dotfiles repo from github"
-git clone https://github.com/jasonventresca/dotfiles ~/dotfiles
+cd ~
+git clone https://github.com/jasonventresca/dotfiles
 
 # Back up existing dotfiles, and then symlink the new dotfiles
 echo "Moving old dotfiles to $old_dir"
+if [ ! -d $old_dir ]; then
+    mkdir $old_dir
+fi
+
 for file in $files; do
-    mv ~/$file $old_dir
-    echo "Creating symlink to $file in home directory"
-    ln -s $dir/$file ~/$file
+	if [ -e ~/.$file ]; then
+    	mv ~/.$file $old_dir/.$file
+	fi
+    echo "Creating symlink ~/.$file  -->  $dir/$file
+    ln -s $dir/$file ~/.$file
 done
 
 # Keep EC2 connections from periodically hanging up
@@ -34,5 +37,5 @@ sudo echo "KeepAlive yes" >> /etc/ssh/sshd_config
 sudo echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
 
 # Done!
-echo "All done! Log out of all open sessions to install new env!"
+echo "All done! Log out of all open sessions to finish installing new env!"
 
