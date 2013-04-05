0483857be8cc73e8410937c33b940cb79f65b53f
Jason Ventresca <jasonventresca@gmail.com>
2013-04-05 23:01:44 +0000

    added mako template vim stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Use this file to (re)build a familiar command line env
 # run "wget --no-check-certificate https://github.com/jasonventresca/dotfiles/raw/master/bootstrap.sh -O - | sh"
 
@@ -7,6 +9,7 @@
 # Vars
 dir=~/dotfiles/dotfiles
 old_dir=~/dotfiles.old
+vim_mako_dirs="ftdetect indent syntax"
 files="bashrc gitconfig tmux.conf vim vimrc"
 install_tools_script="~/dotfiles/install_tools.sh"
 
@@ -20,6 +23,7 @@ fi
 # Install these dotfiles locally
 echo "Cloning dotfiles repo from github"
 git clone https://github.com/jasonventresca/dotfiles ~/dotfiles
+git clone git://github.com/sophacles/vim-bundle-mako.git ~/dotfiles/vim-bundle-mako
 
 # Back up existing dotfiles, and then symlink the new dotfiles
 echo "Moving old dotfiles to $old_dir"
@@ -28,12 +32,21 @@ if [ ! -d $old_dir ]; then
 fi
 
 for file in $files; do
-	if [ -e ~/.$file ]; then
-    	mv ~/.$file $old_dir/.$file
-	fi
+    if [ -e ~/.$file ]; then
+        mv ~/.$file $old_dir/.$file
+    fi
     echo "Creating symlink ~/.$file  -->  $dir/$file"
     ln -s $dir/$file ~/.$file
 done
+
+for subdir in $vim_mako_dirs; do
+    if [ -e ~/.vim/$subdir/mako.vim ]; then
+        mv ~/.vim/$subdir/mako.vim $old_dir/.vim/$subdir/mako.vim
+    fi
+    echo "Creating symlink ~/dotfiles/vim-bundle-mako/$subdir/mako.vim  -->   ~/.vim/$subdir/mako.vim"
+    ln -s ~/dotfiles/vim-bundle-mako/$subdir/mako.vim ~/.vim/$subdir/mako.vim
+done
+
 
 # Keep EC2 connections from periodically hanging up
 echo "Configuring global SSH settings"
