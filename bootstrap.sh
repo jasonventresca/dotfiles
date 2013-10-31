948c032fc7e5fd19e834d5dcaeba7253e3546418
Jason Ventresca <jasonventresca@gmail.com>
2013-10-31 21:29:40 +0000

    ok...

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,54 +8,56 @@ set -eux
 # Need to sudo to sucessfully apt-get
 
 # Vars
-dir=~/dotfiles/dotfiles
-old_dir=~/dotfiles.old
+dir=$HOME/dotfiles/dotfiles
+old_dir=$HOME/dotfiles.old
 vim_mako_dirs="ftdetect indent syntax"
 files="bashrc gitconfig tmux.conf vim vimrc"
 
 # first install git
 sudo apt-get install -y git-core
 
-mkdir -p ~/dotfiles
+mkdir -p $HOME/dotfiles
 
 # Install these dotfiles locally
 echo "Cloning dotfiles repo from github"
-git clone git@github.com:jasonventresca/dotfiles.git ~/dotfiles
-git clone git://github.com/sophacles/vim-bundle-mako.git ~/dotfiles/vim-bundle-mako
+git clone git@github.com:jasonventresca/dotfiles.git $HOME/dotfiles
+git clone git://github.com/sophacles/vim-bundle-mako.git $HOME/dotfiles/vim-bundle-mako
 
 echo "Installing dev tools"
-~/dotfiles/install_tools.sh
+$HOME/dotfiles/install_tools.sh
 
 # Backup existing dotfiles, and then symlink the new dotfiles
 echo "Moving old dotfiles to $old_dir"
 mkdir -p $old_dir
 for file in $files; do
-    if [ -e ~/.$file ]; then
-        mv ~/.$file $old_dir/.$file
+    if [ -e $HOME/.$file ]; then
+        mv $HOME/.$file $old_dir/.$file
     fi
-    echo "Creating symlink ~/.$file  -->  $dir/$file"
-    ln -s $dir/$file ~/.$file
+    echo "Creating symlink $HOME/.$file  -->  $dir/$file"
+    ln -s $dir/$file $HOME/.$file
 done
 
 # for Mac, symlink ~/.profile to point to ~/.bashrc
-if [ -e ~/.profile ]; then
+if [ -e $HOME/.profile ]; then
     mv .profile $old_dir/.profile # backup
-    echo "Creating symlink ~/.profile --> $dir/bashrc"
-    ln -s $dir/bashrc ~/.profile
+    echo "Creating symlink $HOME/.profile --> $dir/bashrc"
+    ln -s $dir/bashrc $HOME/.profile
 fi
 
 
+set -eux
+vim_mako_dirs="ftdetect indent syntax"
 for subdir in $vim_mako_dirs; do
-    file="~/.vim/$subdir/mako.vim"
+    file="/home/ubuntu/.vim/$subdir/mako.vim"
     if [ -e $file ]; then
         mkdir -p $old_dir/.vim/$subdir
         mv $file $old_dir/.vim/$subdir/mako.vim
     else
-        mkdir -p ~/.vim/$subdir
+        mkdir -p /home/ubuntu/.vim/$subdir
     fi
 
-    echo "Creating symlink ~/.vim/$subdir/mako.vim --> ~/dotfiles/vim-bundle-mako/$subdir/mako.vim"
-    ln -s ~/dotfiles/vim-bundle-mako/$subdir/mako.vim $file
+    echo "Creating symlink $HOME/.vim/$subdir/mako.vim --> $HOME/dotfiles/vim-bundle-mako/$subdir/mako.vim"
+    ln -s /home/ubuntu/dotfiles/vim-bundle-mako/$subdir/mako.vim $file
 done
 
 
