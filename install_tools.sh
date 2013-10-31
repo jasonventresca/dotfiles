cf8b2b30081cf063270a69a618389b3d37486e92
Jason Ventresca <jasonventresca@gmail.com>
2013-10-31 20:53:50 +0000

    some small fixes

--- install_tools.sh
+++ install_tools.sh
@@ -3,6 +3,5 @@ set -eux
 
 # dev tools
 sudo apt-get install -y vim git-core tig tmux build-essential bash-completion \
-                        sl curl ipython python-setuptools python-dev python-pip \
-                        pygmentize
+                        sl curl ipython python-setuptools python-dev python-pip
 
