fe35920d0dd9d5a0f8968116539a77120a7c9a36
Jason <jasonventresca@gmail.com>
2015-08-04 20:54:37 +0000

    cut out some of the cruft in the install process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
-# Use this script to (re)build a familiar command line env
-
-# Don't forget the SSH keys.
-# Need to sudo to sucessfully apt-get
-
-# first install git
 sudo apt-get install -y git-core || brew install git
 
-# Install these dotfiles locally
-mkdir -p $HOME/dotfiles
-echo "Cloning dotfiles repo from github"
 git clone git@github.com:jasonventresca/dotfiles.git $HOME/dotfiles
-git clone git://github.com/sophacles/vim-bundle-mako.git $HOME/dotfiles/vim-bundle-mako
 
 $HOME/dotfiles/install.sh
--- install_tools.sh
+++ install_tools.sh
@@ -2,11 +2,11 @@
 set -eu
 
 install_deb(){
-    sudo apt-get install -y vim git-core tig tmux build-essential bash-completion sl curl ipython python-setuptools python-dev python-pip
+    sudo apt-get install -y vim git-core tmux build-essential bash-completion sl curl
 }
 
 install_mac(){
-    brew install vim git tig tmux bash-completion sl curl coreutils
+    brew install vim git tmux bash-completion sl curl coreutils
     sudo easy_install pip
 }
 
