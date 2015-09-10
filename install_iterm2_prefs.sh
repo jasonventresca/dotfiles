c2857c538db017663226bd45503e0b2322882535
Jason <jasonventresca@gmail.com>
2015-09-10 12:01:54 -0400

    new dotfiles location

--- install_iterm2_prefs.sh
+++ install_iterm2_prefs.sh
@@ -4,4 +4,4 @@ set -eux
 # iTerm2 not taking newly installed plist?
 #   http://apple.stackexchange.com/a/111559
 
-cp ~/dotfiles/other_dotfiles/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+cp ~/dotfiles.jason_ventresca/other_dotfiles/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
