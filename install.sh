70ecc5eb22733ed45842027386937d79b49ff566
Jason <jasonventresca@gmail.com>
2015-11-19 12:59:07 -0500

    hopefull this works for both Mac OS X and Linux.

--- install.sh
+++ install.sh
@@ -10,10 +10,11 @@ if uname | grep -i darwin >/dev/null ; then
     echo "Mac OS X detected. Symlinking $HOME/.profile => $REPO/dotfiles/profile"
     rm -f $HOME/.profile
     ln -s $REPO/dotfiles/profile $HOME/.profile
+elif uname | grep -i linux >/dev/null ; then
+    append_str='[[ -n $LC_jason_ventresca ]] && source $HOME/dotfiles.jason_ventresca/dotfiles/bashrc'
+    if ! grep -F "$append_str" $HOME/.bash_profile >/dev/null ; then
+        echo "Linux detected. Appending magic to $HOME/.bash_profile..."
+        echo $append_str >>$HOME/.bash_profile
+    fi
 fi
 
-append_str='[[ -n $LC_jason_ventresca ]] && source $HOME/dotfiles.jason_ventresca/dotfiles/bashrc'
-if ! grep -F "$append_str" $HOME/.bash_profile >/dev/null ; then
-    echo "Appending magic to $HOME/.bash_profile..."
-    echo $append_str >>$HOME/.bash_profile
-fi
