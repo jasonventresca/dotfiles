39e4c0cab2ce254a35169df4039748f9b5e3602c
Jason <jasonventresca@gmail.com>
2016-01-06 19:43:36 -0500

    added custom ssh port remote_install_dotfiles

--- remote_install_dotfiles.sh
+++ remote_install_dotfiles.sh
@@ -2,5 +2,6 @@
 set -eu
 
 HOST=$1
+PORT=$2
 
-ssh -o PermitLocalCommand=no ubuntu@$HOST 'wget --no-check-certificate https://github.com/jasonventresca/dotfiles/raw/master/bootstrap.sh -O - | bash'
+ssh -o PermitLocalCommand=no ubuntu@$HOST -p $PORT 'wget --no-check-certificate https://github.com/jasonventresca/dotfiles/raw/master/bootstrap.sh -O - | bash'
