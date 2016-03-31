b19ea4430b957cbedbe4a302ce4811f0416ea560
Jason <jasonventresca@gmail.com>
2016-03-31 12:25:53 -0400

    whoops

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if uname | grep -i darwin >/dev/null ; then
     rm -f $HOME/.profile
     ln -s $REPO/dotfiles/profile $HOME/.profile
 elif uname | grep -i linux >/dev/null ; then
-    append_str='[[ -n $LC_jason_ventresca ]] && source $HOME/dotfiles.jason_ventresca/dotfiles/bashrc'
+    append_str='([[ -n $LC_jason_ventresca ]] && source $HOME/dotfiles.jason_ventresca/dotfiles/bashrc) || source $HOME/.bashrc'
     if ! grep -F "$append_str" $HOME/.bash_profile >/dev/null ; then
         echo "Linux detected. Appending magic to $HOME/.bash_profile..."
         echo $append_str >>$HOME/.bash_profile
