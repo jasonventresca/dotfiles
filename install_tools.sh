13a107dbf9b79c876981fad263e9c2b941223e7e
Jason <jasonventresca@gmail.com>
2016-07-18 13:50:03 -0400

    smarter ssh config for amazon VPC, and pip install ropevim deps

--- install_tools.sh
+++ install_tools.sh
@@ -30,6 +30,10 @@ install_platform_agnostic(){
     mkdir -p $VIM_DIR/{autoload,bundle} && \
         curl -LSso $VIM_DIR/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+    # install python libraries that the rope vim plugin will import
+    # https://github.com/python-rope/rope
+    sudo pip install rope ropevim
+
     # install Vim plugin for rope python tools
     cd $VIM_DIR/bundle && git clone 'https://github.com/python-rope/ropevim.git'
 }
