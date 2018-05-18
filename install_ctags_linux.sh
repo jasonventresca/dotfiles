f797031d2448c207de4be1112193df5bea10a7a8
Jason <jasonventresca@gmail.com>
2018-05-18 19:53:18 +0000

    just install for linux, brew installs it for mac

--- install_ctags_linux.sh
+++ install_ctags_linux.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 set -eu
 
-REPO=$HOME/dotfiles.jason_ventresca
+# https://github.com/universal-ctags/ctags
 
-source $REPO/util/platform.sh
-
-function install_linux() {
-    (
-        cd $HOME
-        git clone 'https://github.com/universal-ctags/ctags.git'
-        cd ctags
-        ./autogen.sh
-        ./configure
-        make
-        sudo make install
-        cd $HOME
-        rm -rf ctags
-    )
-}
-
-if is_macOS ; then
-    true # TODO
-
-elif is_Linux ; then
-    install_linux
-fi
+(
+    cd $HOME
+    git clone 'https://github.com/universal-ctags/ctags.git'
+    cd ctags
+    ./autogen.sh
+    ./configure
+    make
+    sudo make install
+    cd $HOME
+    rm -rf ctags
+)
