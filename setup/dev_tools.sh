6b1b92765fa5984563593984ffe74d5b528a3a0a
Jason <jasonventresca@gmail.com>
2018-10-03 17:52:23 -0400

    pipenv bin dir in PATH, and install node for debian

--- setup/dev_tools.sh
+++ setup/dev_tools.sh
@@ -20,6 +20,10 @@ install_deb() {
         npm \
         tree
 
+    # https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+
     sudo pip install Pygments
 
     sudo $REPO/setup/debian/fpp.sh
