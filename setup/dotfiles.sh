3077e56a7b6882f3c81a5756c8805a62e92e3178
Jason Ventresca <jason.ventresca@oracle.com>
2023-04-21 15:48:45 -0400

    Some SSH servers no longer accept LC_* env vars by default

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -11,7 +11,7 @@ if is_macOS ; then
     ln -s $REPO/dotfiles/profile $HOME/.profile
 
 elif is_Linux ; then
-    append_str='[[ -n $LC_jason_ventresca ]] && source $HOME/dotfiles.jason_ventresca/dotfiles/bashrc || source $HOME/.bashrc'
+    append_str='[[ -n $LC_jason_ventresca || -n $AWS_jason_ventresca ]] && source $HOME/dotfiles.jason_ventresca/dotfiles/bashrc || source $HOME/.bashrc'
     if ! grep -F "$append_str" $HOME/.bash_profile >/dev/null ; then
         echo "Linux detected. Appending magic to $HOME/.bash_profile..."
         echo $append_str >>$HOME/.bash_profile
