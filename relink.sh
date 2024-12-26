a3c7a8c133da246b2cec5242a7a9165a392b1d47
Jason Ventresca <jasonventresca@gmail.com>
2024-12-26 12:53:19 -0700

    getting my bashrc back!

--- relink.sh
+++ relink.sh
@@ -5,7 +5,7 @@
 # Vars
 dir=~/dotfiles.jason_ventresca/dotfiles
 old_dir=~/dotfiles.old
-files="gitconfig inputrc tmux.conf vim vimrc zshrc"
+files="gitconfig inputrc tmux.conf vim vimrc bashrc"
 
 for file in $files; do
   if [ -e ~/.$file ]; then
